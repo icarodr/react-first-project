4ab8524863e0e698e605f3467255840957f2897a
icarodr <it46009@gmail.com>
2022-07-14 18:17:51 -0300

    .

---
+++ src/Demo.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+function Demo() {
+  return <Button variant="contained"> Olá Mundo</Button>;
+}
+
+export default Demo;
