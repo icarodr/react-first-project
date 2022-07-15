8d181a554233f2e2eb954bb98b51389febe635ff
icarodr <it46009@gmail.com>
2022-07-15 00:17:27 -0300

    add the next steps

--- src/Demo.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-
-function Demo() {
-  return <Button variant="contained"> Olá Mundo</Button>;
-}
-
-export default Demo;
